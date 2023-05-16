fd1987f908cb21cda3184f0bd9f30981517c01e6
gaudoux <gaspard.audoux@etudiant.univ-rennes1.fr>
2023-05-16 15:53:40 +0200

    erreur corrigé

--- Project_prog2/src/main/ColisionVerif.java
+++ Project_prog2/src/main/ColisionVerif.java
@@ -8,19 +8,31 @@ public class ColisionVerif {
 	public ColisionVerif(GamePanel gp) {
 
 	}
-	
+
 	public void checkTile(Entity entity) {
-		int x_g=entity.getM_x()+entity.area_collision.x;
-		int x_d=entity.getM_x()+entity.area_collision.x+entity.area_collision.width;
-		int y_g=entity.getM_y()+entity.area_collision.y;
-		int y_d=entity.getM_y()+entity.area_collision.y+entity.area_collision.height;
-		
-		int entityLeftCol =x_g/gp.TILE_SIZE;
-		int entityRightCol =x_g/gp.TILE_SIZE;
-		int entityTopRow =y_g/gp.TILE_SIZE;
-		int entityBottomRow=y_d/gp.TILE_SIZE;
-		
-		int tile1,tile2;
-		
+		int x_g = entity.getM_x() + entity.area_collision.x;
+		int x_d = entity.getM_x() + entity.area_collision.x + entity.area_collision.width;
+		int y_g = entity.getM_y() + entity.area_collision.y;
+		int y_d = entity.getM_y() + entity.area_collision.y + entity.area_collision.height;
+
+		int entityLeftCol = x_g / gp.TILE_SIZE;
+		int entityRightCol = x_g / gp.TILE_SIZE;
+		int entityTopRow = y_g / gp.TILE_SIZE;
+		int entityBottomRow = y_d / gp.TILE_SIZE;
+
+		int tile1, tile2;
+
+		if (entity.direction == "up") {
+//entityTopRow = gp.til
+		}
+		if (entity.direction == "right") {
+
+		}
+		if (entity.direction == "left") {
+
+		}
+		if (entity.direction == "down") {
+
+		}
 	}
 }
