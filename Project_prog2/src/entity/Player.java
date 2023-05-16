415ca6dd3186c4eb11b11ab6ab65809ec7bfad29
gaudoux <gaspard.audoux@etudiant.univ-rennes1.fr>
2023-05-16 17:58:21 +0200

    Merge branch 'master' of https://github.com/godouxx/Project_prog2.git

--- Project_prog2/src/entity/Player.java
+++ Project_prog2/src/entity/Player.java
@@ -17,8 +17,7 @@ import tile.Tile;
  *
  */
 public class Player extends Entity {
-	
-	
+
 	GamePanel m_gp;
 	KeyHandler m_keyH;
 	BufferedImage heart_empty;
@@ -28,7 +27,6 @@ public class Player extends Entity {
 
 	public final int screenX;
 	public final int screenY;
-
 
 	/**
 	 * Constructeur de Player
@@ -41,15 +39,14 @@ public class Player extends Entity {
 		this.m_keyH = a_keyH;
 		this.setDefaultValues();
 		this.getPlayerImage();
-		this.area_collision=new Rectangle(0,0,46,46);
+		this.area_collision = new Rectangle(0, 0, 46, 46);
 		this.pvMAX = 6;
 		this.pvACTUAL = this.pvMAX;
 		this.getHeartImage();
 		this.getGameOver();
-		screenX=a_gp.SCREEN_WIDTH/2-(a_gp.TILE_SIZE/2);
-		screenY=a_gp.SCREEN_HEIGHT/2-(a_gp.TILE_SIZE/2);
+		screenX = a_gp.SCREEN_WIDTH / 2 - (a_gp.TILE_SIZE / 2);
+		screenY = a_gp.SCREEN_HEIGHT / 2 - (a_gp.TILE_SIZE / 2);
 	}
-	
 
 	/**
 	 * Initialisation des donnï¿½es membres avec des valeurs par dï¿½faut
@@ -71,21 +68,20 @@ public class Player extends Entity {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	public void getHeartImage() {
 		try {
 			heart_empty = ImageIO.read(getClass().getResource("/maps/heart_red_empty.png"));
-			
+
 			heart_half = ImageIO.read(getClass().getResource("/maps/heart_red_half.png"));
-			
+
 			heart_full = ImageIO.read(getClass().getResource("/maps/heart_red_full.png"));
-					
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void getGameOver() {
 		try {
 			game_over = ImageIO.read(getClass().getResource("/ecran_fin/game_over.png"));
@@ -93,113 +89,105 @@ public class Player extends Entity {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void over(Graphics2D a_g2) {
-		a_g2.drawImage(game_over, m_gp.SCREEN_WIDTH/4 , m_gp.SCREEN_HEIGHT/4, 400 , 400 ,  null);
+		a_g2.drawImage(game_over, m_gp.SCREEN_WIDTH / 4, m_gp.SCREEN_HEIGHT / 4, 400, 400, null);
 	}
-	
+
 	/**
 	 * Mise ï¿½ jour des donnï¿½es du joueur
 	 */
 	@Override
 	public void update() {
-		
-	    
 
-	    for (int element : m_keyH.liste) {
-	        System.out.println(element);
-	        if (element == 90) {
-	            goUp = true;
-	        }
-	        if (element == 83) {
-	            goDown = true;
-	        }
-	        if (element == 81) {
-	            goLeft = true;
-	        }
-	        if (element == 68) {
-	            goRight = true;
-	        }
-	        
-	        
-	        
-	        collision=false;
-	        this.m_gp.colisionVerif.checkTile(this);
+		for (int element : m_keyH.liste) {
+			System.out.println(element);
+			if (element == 90) {
+				goUp = true;
+			}
+			if (element == 83) {
+				goDown = true;
+			}
+			if (element == 81) {
+				goLeft = true;
+			}
+			if (element == 68) {
+				goRight = true;
+			}
 
-	    }
-	   
-	    if(collision==false) {
-	    	
-	    	 // Déplacement diagonal
-		    
-		    if (goUp && goLeft) {
-		        goUpLeftNext();
-		    } else if (goUp && goRight) {
-		        goUpRightNext();
-		    } else if (goDown && goLeft) {
-		        goDownLeftNext();
-		    } else if (goDown && goRight) {
-		        goDownRightNext();
-		    }
-		    // Dï¿½placement vertical ou horizontal
-		    else if (goUp) {
-		        goUpNext();
-		    } else if (goDown) {
-		        goDownNext();
-		    } else if (goLeft) {
-		        goLeftNext();
-		    } else if (goRight) {
-		        goRightNext();
-		    }
-		    
-	    }
-	   
-	    
-	    goUp=false;
-	    goDown=false;
-	    goLeft=false;
-	    goRight=false;
+			collision = false;
+			this.m_gp.colisionVerif.checkTile(this);
+
+		}
+
+		if (collision == false) {
+
+			// Déplacement diagonal
+
+			if (goUp && goLeft) {
+				goUpLeftNext();
+			} else if (goUp && goRight) {
+				goUpRightNext();
+			} else if (goDown && goLeft) {
+				goDownLeftNext();
+			} else if (goDown && goRight) {
+				goDownRightNext();
+			}
+			// Dï¿½placement vertical ou horizontal
+			else if (goUp) {
+				goUpNext();
+			} else if (goDown) {
+				goDownNext();
+			} else if (goLeft) {
+				goLeftNext();
+			} else if (goRight) {
+				goRightNext();
+			}
+
+		}
+
+		goUp = false;
+		goDown = false;
+		goLeft = false;
+		goRight = false;
 	}
 
-
 	public void goUpNext() {
-		this.m_y -= 1*m_speed;
+		this.m_y -= 1 * m_speed;
 	}
 
 	public void goDownNext() {
-		this.m_y += 1*m_speed;
+		this.m_y += 1 * m_speed;
 	}
 
-	
 	public void goLeftNext() {
-		this.m_x -= 1*m_speed;
+		this.m_x -= 1 * m_speed;
 	}
 
 	public void goRightNext() {
-		this.m_x += 1*m_speed;
+		this.m_x += 1 * m_speed;
 	}
 
 	public void goUpLeftNext() {
-	    this.m_y -= Math.sqrt(2)/2 * m_speed;
-	    this.m_x -= Math.sqrt(2)/2 * m_speed;
+		this.m_y -= Math.sqrt(2) / 2 * m_speed;
+		this.m_x -= Math.sqrt(2) / 2 * m_speed;
 	}
 
 	public void goUpRightNext() {
-	    this.m_y -= Math.sqrt(2)/2 * m_speed;
-	    this.m_x += Math.sqrt(2)/2 * m_speed;
+		this.m_y -= Math.sqrt(2) / 2 * m_speed;
+		this.m_x += Math.sqrt(2) / 2 * m_speed;
 	}
 
 	public void goDownLeftNext() {
-	    this.m_y += Math.sqrt(2)/2 * m_speed;
-	    this.m_x -= Math.sqrt(2)/2 * m_speed;
+		this.m_y += Math.sqrt(2) / 2 * m_speed;
+		this.m_x -= Math.sqrt(2) / 2 * m_speed;
 	}
 
 	public void goDownRightNext() {
-	    this.m_y += Math.sqrt(2)/2 * m_speed;
-	    this.m_x += Math.sqrt(2)/2 * m_speed;
+		this.m_y += Math.sqrt(2) / 2 * m_speed;
+		this.m_x += Math.sqrt(2) / 2 * m_speed;
 	}
 
-	
 	/**
 	 * Affichage du l'image du joueur dans la fenï¿½tre du jeu
 	 * 
@@ -212,44 +200,42 @@ public class Player extends Entity {
 		// affiche le personnage avec l'image "image", avec les coordonnï¿½es x et y, et
 		// de taille tileSize (16x16) sans ï¿½chelle, et 48x48 avec ï¿½chelle)
 		a_g2.drawImage(l_image, screenX, screenY, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
-		
-		switch(this.getPvACTUAL()) {
+
+		switch (this.getPvACTUAL()) {
 		case 6:
-			a_g2.drawImage(heart_full , 20, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
-			a_g2.drawImage(heart_full , 70, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
-			a_g2.drawImage(heart_full , 120, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_full, 20, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_full, 70, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_full, 120, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
 			break;
 		case 5:
-			a_g2.drawImage(heart_full , 20, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
-			a_g2.drawImage(heart_full , 70, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
-			a_g2.drawImage(heart_half , 120, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_full, 20, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_full, 70, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_half, 120, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
 			break;
 		case 4:
-			a_g2.drawImage(heart_full , 20, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
-			a_g2.drawImage(heart_full , 70, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
-			a_g2.drawImage(heart_empty , 120, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_full, 20, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_full, 70, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_empty, 120, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
 			break;
 		case 3:
-			a_g2.drawImage(heart_full , 20, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
-			a_g2.drawImage(heart_half , 70, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
-			a_g2.drawImage(heart_empty , 120, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_full, 20, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_half, 70, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_empty, 120, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
 			break;
 		case 2:
-			a_g2.drawImage(heart_full , 20, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
-			a_g2.drawImage(heart_empty , 70, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
-			a_g2.drawImage(heart_empty , 120, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_full, 20, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_empty, 70, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_empty, 120, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
 			break;
 		case 1:
-			a_g2.drawImage(heart_half , 20, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
-			a_g2.drawImage(heart_empty , 70, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
-			a_g2.drawImage(heart_empty , 120, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_half, 20, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_empty, 70, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+			a_g2.drawImage(heart_empty, 120, 8, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
 			break;
 		default:
-			a_g2.drawImage(game_over, m_gp.SCREEN_WIDTH/2 , m_gp.SCREEN_HEIGHT/2, 200 , 200 ,  null);
-	
+			a_g2.drawImage(game_over, m_gp.SCREEN_WIDTH / 2, m_gp.SCREEN_HEIGHT / 2, 200, 200, null);
+
 		}
-		
-		
 
 	}
 
--- Project_prog2/src/main/ColisionVerif.java
+++ Project_prog2/src/main/ColisionVerif.java
@@ -23,6 +23,7 @@ public class ColisionVerif {
 		int entityBottomRow = y_d / gp.TILE_SIZE;
 
 		int tile1, tile2;
+//on verifie si on est en collision avec un obstacle en bas a gauche
 		if (entity.goDown == true && entity.goLeft) {
 			entityBottomRow = (y_d + entity.getM_speed()) / gp.TILE_SIZE;
 			entityLeftCol = (x_g - entity.getM_speed()) / gp.TILE_SIZE;
@@ -32,8 +33,9 @@ public class ColisionVerif {
 				System.out.println("On entre en collsion avec un obstacle vers le bas et la gauche");
 				entity.collision = true;
 			}
-
-		} else if (entity.goDown && entity.goRight) {
+		} 
+		//on verifie si on est en collision avec un obstacle en bas a droite
+		else if (entity.goDown && entity.goRight) {
 			entityBottomRow = (y_d + entity.getM_speed()) / gp.TILE_SIZE;
 			entityRightCol = (x_d - entity.getM_speed()) / gp.TILE_SIZE;
 			tile1 = gp.m_tileM.m_mapTileNum[entityLeftCol][entityBottomRow];
@@ -42,59 +44,77 @@ public class ColisionVerif {
 				System.out.println("On entre en collsion avec un obstacle vers le bas et la droite");
 				entity.collision = true;
 			}
-		} else if (entity.goUp && entity.goRight) {
+		} 
+		// On vÈrifie si on est en collision avec un obstacle en haut ‡ gauche
+		else if (entity.goUp && entity.goLeft) {
+		    entityTopRow = (y_g - entity.getM_speed()) / gp.TILE_SIZE; // Mettez ‡ jour y_g avec une soustraction
+		    entityLeftCol = (x_g - entity.getM_speed()) / gp.TILE_SIZE;
+		    entityRightCol = (x_d - entity.getM_speed()) / gp.TILE_SIZE;
+		    tile1 = gp.m_tileM.m_mapTileNum[entityLeftCol][entityTopRow];
+		    tile2 = gp.m_tileM.m_mapTileNum[entityRightCol][entityTopRow];
+		    if (gp.m_tileM.m_tile[tile1].m_collision || gp.m_tileM.m_tile[tile2].m_collision) {
+		        System.out.println("On entre en collision avec un obstacle vers le haut et la gauche");
+		        entity.collision = true;
+		    }
+		} 
+
+		// On vÈrifie si on est en collision avec un obstacle en haut ‡ droite
+		else if (entity.goUp && entity.goRight) {
+		    entityTopRow = (y_g - entity.getM_speed()) / gp.TILE_SIZE; // Mettez ‡ jour y_g avec une soustraction
+		    entityLeftCol = (x_g + entity.getM_speed()) / gp.TILE_SIZE; // Mettez ‡ jour x_g avec une addition
+		    entityRightCol = (x_d + entity.getM_speed()) / gp.TILE_SIZE; // Mettez ‡ jour x_d avec une addition
+		    tile1 = gp.m_tileM.m_mapTileNum[entityLeftCol][entityTopRow];
+		    tile2 = gp.m_tileM.m_mapTileNum[entityRightCol][entityTopRow];
+		    if (gp.m_tileM.m_tile[tile1].m_collision || gp.m_tileM.m_tile[tile2].m_collision) {
+		        System.out.println("On entre en collision avec un obstacle vers le haut et la droite");
+		        entity.collision = true;
+		    }
+		}
+
+
+		//on v√©rifie si on est en collision avec un obstacle en haut
+		else if (entity.goUp) {
+
 			entityTopRow = (y_g + entity.getM_speed()) / gp.TILE_SIZE;
-			entityRightCol = (x_d - entity.getM_speed()) / gp.TILE_SIZE;
 			tile1 = gp.m_tileM.m_mapTileNum[entityLeftCol][entityTopRow];
 			tile2 = gp.m_tileM.m_mapTileNum[entityRightCol][entityTopRow];
 			if (gp.m_tileM.m_tile[tile1].m_collision == true || gp.m_tileM.m_tile[tile2].m_collision == true) {
-				System.out.println("On entre en collsion avec un obstacle vers le haut et la droite");
+				System.out.println("On entre en collsion avec un obstacle vers le haut");
 				entity.collision = true;
-			}
-		} else if (entity.goUp && entity.goLeft) {
-			entityTopRow = (y_g + entity.getM_speed()) / gp.TILE_SIZE;
-			entityLeftCol = (x_g - entity.getM_speed()) / gp.TILE_SIZE;
-			tile1 = gp.m_tileM.m_mapTileNum[entityLeftCol][entityTopRow];
-			tile2 = gp.m_tileM.m_mapTileNum[entityRightCol][entityTopRow];
-			if (gp.m_tileM.m_tile[tile1].m_collision == true || gp.m_tileM.m_tile[tile2].m_collision == true) {
-				System.out.println("On entre en collsion avec un obstacle vers le haut et la gauche");
-				entity.collision = true;
-			} else if (entity.goUp == true) {
-
-				entityTopRow = (y_g + entity.getM_speed()) / gp.TILE_SIZE;
-				tile1 = gp.m_tileM.m_mapTileNum[entityLeftCol][entityTopRow];
-				tile2 = gp.m_tileM.m_mapTileNum[entityRightCol][entityTopRow];
-				if (gp.m_tileM.m_tile[tile1].m_collision == true || gp.m_tileM.m_tile[tile2].m_collision == true) {
-					System.out.println("On entre en collsion avec un obstacle vers le haut");
-					entity.collision = true;
-				}
-			} else if (entity.goRight == true) {
-				entityRightCol = (x_d - entity.getM_speed()) / gp.TILE_SIZE;
-				tile1 = gp.m_tileM.m_mapTileNum[entityRightCol][entityTopRow];
-				tile2 = gp.m_tileM.m_mapTileNum[entityRightCol][entityBottomRow];
-				if (gp.m_tileM.m_tile[tile1].m_collision == true || gp.m_tileM.m_tile[tile2].m_collision == true) {
-					System.out.println("On entre en collsion avec un obstacle vers la droite");
-					entity.collision = true;
-				}
-			} else if (entity.goLeft == true) {
-				entityLeftCol = (x_g - entity.getM_speed()) / gp.TILE_SIZE;
-				tile1 = gp.m_tileM.m_mapTileNum[entityLeftCol][entityTopRow];
-				tile2 = gp.m_tileM.m_mapTileNum[entityLeftCol][entityBottomRow];
-				if (gp.m_tileM.m_tile[tile1].m_collision == true || gp.m_tileM.m_tile[tile2].m_collision == true) {
-					System.out.println("On entre en collsion avec un obstacle vers la gauche");
-					entity.collision = true;
-				}
-			} else if (entity.goDown == true) {
-				entityBottomRow = (y_d + entity.getM_speed()) / gp.TILE_SIZE;
-				tile1 = gp.m_tileM.m_mapTileNum[entityLeftCol][entityBottomRow];
-				tile2 = gp.m_tileM.m_mapTileNum[entityRightCol][entityBottomRow];
-				if (gp.m_tileM.m_tile[tile1].m_collision == true || gp.m_tileM.m_tile[tile2].m_collision == true) {
-					System.out.println("On entre en collsion avec un obstacle vers le bas");
-					entity.collision = true;
-				}
-
 			}
 		}
+		//on v√©rifie si on est en collision avec un obstacle √† droite
+		else if (entity.goRight) {
+			entityRightCol = (x_d - entity.getM_speed()) / gp.TILE_SIZE;
+			tile1 = gp.m_tileM.m_mapTileNum[entityRightCol][entityTopRow];
+			tile2 = gp.m_tileM.m_mapTileNum[entityRightCol][entityBottomRow];
+			if (gp.m_tileM.m_tile[tile1].m_collision == true || gp.m_tileM.m_tile[tile2].m_collision == true) {
+				System.out.println("On entre en collsion avec un obstacle vers la droite");
+				entity.collision = true;
+			}
+		} 
+		//on v√©rifie si on est en collision avec un obstacle √† gauche
+		else if (entity.goLeft == true) {
+			entityLeftCol = (x_g - entity.getM_speed()) / gp.TILE_SIZE;
+			tile1 = gp.m_tileM.m_mapTileNum[entityLeftCol][entityTopRow];
+			tile2 = gp.m_tileM.m_mapTileNum[entityLeftCol][entityBottomRow];
+			if (gp.m_tileM.m_tile[tile1].m_collision == true || gp.m_tileM.m_tile[tile2].m_collision == true) {
+				System.out.println("On entre en collsion avec un obstacle vers la gauche");
+				entity.collision = true;
+			}
+		} 
+		//on v√©rifie si on est en collision avec un obstacle en bas
+		else if (entity.goDown == true) {
+			entityBottomRow = (y_d + entity.getM_speed()) / gp.TILE_SIZE;
+			tile1 = gp.m_tileM.m_mapTileNum[entityLeftCol][entityBottomRow];
+			tile2 = gp.m_tileM.m_mapTileNum[entityRightCol][entityBottomRow];
+			if (gp.m_tileM.m_tile[tile1].m_collision == true || gp.m_tileM.m_tile[tile2].m_collision == true) {
+				System.out.println("On entre en collsion avec un obstacle vers le bas");
+				entity.collision = true;
+			}
+
+		}
+
 	}
 
 }
