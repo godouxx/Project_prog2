e5f0b7ca6028f941b6fb065da8f9c0dcb8a57874
emetayer <elise.metayer.22@hotmail.fr>
2023-05-17 08:55:54 +0200

    Merge branch 'master' of https://github.com/godouxx/Project_prog2.git

--- Project_prog2/src/main/CreateComponents.java
+++ Project_prog2/src/main/CreateComponents.java
@@ -16,17 +16,13 @@ public class CreateComponents {
 	public CreateComponents(GamePanel gp) {
 		this.gp = gp;
 	}
-	public void setComponents() {
-		gp.objets.add(new Obstacles(50,100));
-		gp.objets.add(new Armes(2,100,200));
-		gp.objets.add(new Nourriture(2,200,300));
-		gp.objets.add(new Speed(2,300,200));
-		}
 
 	public void setObjetsPassifs() {
 		gp.objets.add(new Armes(armes_infos.damage,gp.TILE_SIZE*armes_infos.position_x,gp.TILE_SIZE*armes_infos.position_y));
 		gp.objets.add(new Obstacles(gp.TILE_SIZE*4,gp.TILE_SIZE*5));
 		gp.objets.add(new Teleporteur(gp.TILE_SIZE*teleporteur_info.position_x,gp.TILE_SIZE*teleporteur_info.position_y));
+		gp.objets.add(new Nourriture(2, gp.TILE_SIZE*12, gp.TILE_SIZE*5));
+		gp.objets.add(new Speed(3, gp.TILE_SIZE*7, gp.TILE_SIZE*10));
 		
 	}
 	
